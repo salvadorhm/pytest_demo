7ea86bcf592d63f9e2350619eae164e4864f43d1
Salvador HM <salvadorhm@gmail.com>
2023-02-09 14:18:25 -0600

    FIXED type: ignore

--- test/test_largest.py
+++ test/test_largest.py
@@ -3,28 +3,28 @@ from largest import LargestNumber as ln
 
 class Testing:
     def test_largestNumber1(self):
-        assert ln.largestNumber(self, 1, 2) == 2
+        assert ln.largestNumber(self, 1, 2) == 2 # type: ignore
 
     def test_largestNumber2(self):
-        assert ln.largestNumber(self, 2, 1) == 2
+        assert ln.largestNumber(self, 2, 1) == 2 # type: ignore
 
     def test_largestNumber3(self):
-        assert ln.largestNumber(self, 2, 2) == None
+        assert ln.largestNumber(self, 2, 2) == None # type: ignore
 
     def test_largestNumber4(self):
-        assert ln.largestNumber(self, -1, 2) == 2
+        assert ln.largestNumber(self, -1, 2) == 2 # type: ignore
 
     def test_largestNumber5(self):
-        assert ln.largestNumber(self, 2, -1) == 2
+        assert ln.largestNumber(self, 2, -1) == 2 # type: ignore
 
     def test_largestNumber6(self):
-        assert ln.largestNumber(self, -1, -1) == None
+        assert ln.largestNumber(self, -1, -1) == None # type: ignore
 
     def test_largestNumber7(self):
-        assert ln.largestNumber(self, -1, -2) == -1
+        assert ln.largestNumber(self, -1, -2) == -1 # type: ignore
 
     def test_largestNumber8(self):
-        assert ln.largestNumber(self, -2, -1) == -1
+        assert ln.largestNumber(self, -2, -1) == -1 # type: ignore
 
     def test_largestNumber9(self):
-        assert type(ln.largestNumber(self, 1, 0)) == int
+        assert type(ln.largestNumber(self, 1, 0)) == int # type: ignore
